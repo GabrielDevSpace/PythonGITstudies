36997d72c2a918e671c1ac5a5234d9f4e76e02d5
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2022-02-01 00:48:03 -0300

    Primeiro commit aulas comentadas

---
+++ Django2/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import ContatoForm
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def contato(request):
+    form = ContatoForm()
+    Context = {
+        'form':form
+    }
+    return render(request,'contato.html', context)
+
+def produto(request):
+    return render(request,'produto.html')
