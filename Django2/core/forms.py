11eec36885a9225b2c63bdd0e699f0c036e29a47
Gabriel Pires <gabrielpirestj@hotmail.com>
2022-02-04 17:32:55 -0300

    Commit

--- Django2/core/forms.py
+++ Django2/core/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 from django.core.mail.message import EmailMessage
 class ContatoForm(forms.Form):
-    name = forms.CharField(label='name')
+    nome = forms.CharField(label='Nome')
     email = forms.EmailField(label='E-mail')
     assunto = forms.CharField(label='Assunto')
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
 
     def send_mail(self):
-        name = self.cleaned_data['name']
+        nome = self.cleaned_data['nome']
         email = self.cleaned_data['email']
         assunto = self.cleaned_data['assunto']
         mensagem = self.cleaned_data['mensagem']
 
-        conteudo = f'Name: {name}\nE-mail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
+        conteudo = f'Name: {nome}\nE-mail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
         mail = EmailMessage(
             subject='E-mail Enviado Pelo Sistema!',
             body=conteudo,
